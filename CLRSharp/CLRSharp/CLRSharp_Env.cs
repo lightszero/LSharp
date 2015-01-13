74869c9db90b713b9c056e986e519efb2db3f5b8
lightszero <lightsever@hotmail.com>
2015-01-13 12:14:51 +0800

    0.31

--- CLRSharp/CLRSharp/CLRSharp_Env.cs
+++ CLRSharp/CLRSharp/CLRSharp_Env.cs
@@ -11,7 +11,7 @@ namespace CLRSharp
         {
             get
             {
-                return "0.30Alpha";
+                return "0.31Alpha";
             }
         }
         public ICLRSharp_Logger logger
