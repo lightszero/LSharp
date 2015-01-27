914f9b43d872afc5b71d643e5e2f96c3753037be
Li Yue <ran.liyue@gmail.com>
2015-01-27 16:32:28 +0800

    Add UnitTest

---
+++ UnitTestDll/Test_ByLynn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitTest;
+using System.Collections;
+
+namespace UnitTestDll
+{
+    class Test_ByLynn
+    {
+        public static void Test_01()
+        {
+            int[] arr = new int[10];
+            int[] arr2 = new int[11];
+
+            if ( arr[0] > arr2[0] )
+            {
+                Logger.Log("1");
+            }
+            else
+            {
+                Logger.Log("2");
+            }
+        }
+    }
+}
