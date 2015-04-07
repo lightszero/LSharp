545939d535da25073058873d101b57e93ac1a15f
diablohsh <huangshuhuai@hotmail.com>
2015-04-07 19:14:12 +0800

    convert object to float on funtion return

--- UnitTestDll/Test_ByDuskforest.cs
+++ UnitTestDll/Test_ByDuskforest.cs
@@ -12,26 +12,20 @@ namespace UnitTestDll
         public static void UnitTest_Bool2Object()
         {
             TestClass.TestObjectArg(true);
-            //fixed
         }
 
         public static void UnitTest_Int2Byte()
         {
             TestClass.TestByteArg(32);
-            //no error
         }
         public static void UnitTest_CtorWithNoArg()
         {
-            //这行代码放在Program.cs里面执行是不会报错的
             System.Activator.CreateInstance(Type.GetType("TestClass,UnitTest"));
-            //error assm
         }
 
         public static void UnitTest_CtorWithByteArg()
         {
-            //这行代码放在Program.cs里面执行是不会报错的
             System.Activator.CreateInstance(Type.GetType("TestClass,UnitTest"), new Object[] { (byte)32 });
-            //error assm
         }
 
         public static void UnitTest_2DimArray()
@@ -39,6 +33,19 @@ namespace UnitTestDll
             int[,] a = new int[1,1];
             a[0, 0] = 4;
             Logger.Log("abc=" + a[0, 0]);
+        }
+
+        private static Object ReturnObject()
+        {
+            return 100;
+        }
+
+        public static void UnitTest_ConvertReturnObject2Float()
+        {
+            if ((float)ReturnObject() != 0)
+            {
+                Logger.Log("100 != 0");
+            }
         }
     }
 }
